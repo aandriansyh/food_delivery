289b673dd933aeb1013256d326406ac64d8103a3
Adhitocho <Adhitocho@gmail.com>
2021-12-26 14:23:14 +0700

    jangan pedulikan ini

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ASUS\AndroidStudioProjects\food_delivery"
+export "FLUTTER_ROOT=C:\Users\Eka Adhitya K\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Eka Adhitya K\StudioProjects\food_delivery"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
